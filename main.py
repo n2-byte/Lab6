13fabd428c29f7803d4ab2f9570081ee79db910e
Nattaya Wedwiroon <nattayawedwiroon12@gmail.com>
2026-01-17 11:17:46 +0700

    Add conditional statements

--- main.py
+++ main.py
@@ -14,3 +14,20 @@ try:
 except:
     print("Invalid")
     quit()
+    
+# check dict
+for x in dictionary_record:
+    if not x.isdigit() or x.startswith("0") or int(x) <= 0:
+        print("Invalid")
+        quit()  
+          
+# check id
+id = str(id)
+if not id.isdigit() or id.startswith("0") or int(id) <= 0:
+    print("Invalid")
+    quit()
+    
+# check property
+if property not in ["artist", "albumTitle", "tracks"]:
+    print("Invalid")
+    quit()
